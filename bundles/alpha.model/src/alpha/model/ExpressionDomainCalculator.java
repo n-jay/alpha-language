a4da3690372590777a8d42909387145fe8daa712
Tomofumi Yuki <tomofumi.yuki@gmail.com>
2017-12-08 18:21:21 +0100

    adding ExpressionDomainCalculator (incomplete)

--- bundles/alpha.model/src/alpha/model/ExpressionDomainCalculator.java
+++ bundles/alpha.model/src/alpha/model/ExpressionDomainCalculator.java
@@ -1,15 +1,72 @@
 package alpha.model;
 
-import org.eclipse.emf.ecore.impl.EObjectImpl;
-
 import alpha.model.util.AbstractAlphaExpressionVisitor;
+import alpha.model.util.AlphaUtil;
+import fr.irisa.cairn.jnimap.isl.jni.JNIISLSet;
 
 public class ExpressionDomainCalculator extends AbstractAlphaExpressionVisitor {
 
 	
 	@Override
 	public void outVariableExpression(VariableExpression ve) {
-		ve.setExpressionDomain(ve.getVariable().getDomain().copy());
-
+		ve.setExpressionDomain(ve.getVariable().getDomain());
+	}
+	
+	@Override
+	public void outConstantExpression(ConstantExpression ce) {
+		ce.setExpressionDomain(AlphaUtil.getScalarDomain(ce));
+	}
+	
+	@Override
+	public void outUnaryExpression(UnaryExpression ue) {
+		ue.setExpressionDomain(ue.getExpr().getExpressionDomain());
+	}
+	
+	@Override
+	public void outBinaryExpression(BinaryExpression be) {
+		//Expression domain of a binary expression is the intersection of the two
+		be.setExpressionDomain(be.getLeft().getExpressionDomain().intersect(be.getRight().getExpressionDomain()));
+	}
+	
+	@Override
+	public void outCaseExpression(CaseExpression ce) {
+		JNIISLSet union = ce.getExprs().stream().map(a->a.getExpressionDomain())
+			.reduce((a,b)->a.union(b)).get();
+		ce.setExpressionDomain(union);
+	}
+	
+	@Override
+	public void outIfExpression(IfExpression ie) {
+		JNIISLSet set = ie.getCondExpr().getExpressionDomain();
+		set = set.intersect(ie.getThenExpr().getExpressionDomain());
+		set = set.intersect(ie.getElseExpr().getExpressionDomain());
+		ie.setExpressionDomain(set);
+	}
+	
+	@Override
+	public void outIndexExpression(IndexExpression ie) {
+		ie.setExpressionDomain(JNIISLSet.buildUniverse(ie.getFunction().getIslMAff().getSpace()));
+	}
+	
+	@Override
+	public void outMultiArgExpression(MultiArgExpression mae) {
+		JNIISLSet intersection = mae.getExprs().stream().map(e->e.getExpressionDomain())
+		.reduce((a,b)->a.intersect(b)).get();
+		mae.setExpressionDomain(intersection);
+	}
+	
+	@Override
+	public void outReduceExpression(ReduceExpression re) {
+		JNIISLSet proj = re.getBody().getExpressionDomain().apply(re.getProjection().getIslMAff().toMap());
+		re.setExpressionDomain(proj);
+	}
+	
+	@Override
+	public void outRestrictExpression(RestrictExpression re) {
+		if (re.getDomainExpr().getType() != POLY_OBJECT_TYPE.SET)
+			throw new RuntimeException("Domain of restrict expression must evaluate to ");
+		
+//		re.getDomainExpr().getISLObject();
+//		re.setExpressionDomain(re.getExpr().getExpressionDomain().intersect());
 	}
 }
--- bundles/alpha.testframework/src/alpha/testframework/Driver.java
+++ bundles/alpha.testframework/src/alpha/testframework/Driver.java
@@ -6,20 +6,23 @@ import org.junit.Test;
 
 import alpha.model.AlphaModelLoader;
 import alpha.model.AlphaRoot;
+import alpha.model.JNIDomainCalculator;
+import alpha.model.util.PrintAST;
 
 public class Driver {
 	
 	
-//	public static void main(String[] args) {
-	@Test
-	public void test() {
+	public static void main(String[] args) {
+//	@Test
+//	public void test() {
 		
 		try {
 			AlphaRoot root = AlphaModelLoader.loadModel("resources/src-alpha/basic/ANN.alpha");
 //			AlphaRoot root = AlphaModelLoader.loadModel("/Users/tyuki/projects/Alpha/alpha-language/bundles/alpha.testframework/resources/src-alpha/basic/ANN.alpha");
 			
+			
 
-			System.out.println(root.getElements());
+			System.out.println(PrintAST.print(root));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
