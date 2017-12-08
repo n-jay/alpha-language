5c5ed6d723afaea0ec9a154c7787c13dc61b5e3c
Tomofumi Yuki <tomofumi.yuki@gmail.com>
2017-12-08 12:10:51 +0100

    adding tests for PrintAST

---
+++ bundles/alpha.testframework/src/alpha/testframework/tests/PrintASTTests.java
@@ -0,0 +1,43 @@
+package alpha.testframework.tests;
+
+import org.junit.Test;
+
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
+
+import alpha.model.AlphaRoot;
+import alpha.model.util.PrintAST;
+import alpha.testframework.AlphaTestTemplate;
+import alpha.testframework.AlphaTestVersion;
+import alpha.testframework.data.AlphaDataFromFileProvider;
+import fr.irisa.cairn.gecos.testframework.ICodeGenerator;
+import fr.irisa.cairn.gecos.testframework.IData;
+import fr.irisa.cairn.gecos.testframework.annotations.ResourcesLocation;
+
+public class PrintASTTests extends AlphaTestTemplate {
+	
+	public PrintASTTests() {
+		doCompile = false;
+	}
+	
+	@Test
+	@UseDataProvider(location = AlphaDataFromFileProvider.class, value = AlphaDataFromFileProvider.PROVIDER_NAME)
+	@ResourcesLocation(bundleName = "alpha.testframework", value = "resources/src-alpha/basic")
+	public void basic(IData d) {
+		runTest(d); 
+	}
+	
+	@Override
+	protected ICodeGenerator<AlphaTestVersion> getGenerator() {
+		return new ICodeGenerator<AlphaTestVersion>() {
+			
+			@Override
+			public void generate(AlphaTestVersion version) {
+				for (AlphaRoot root : version.getAlphaRoots()) {
+					PrintAST.print(root);
+				}
+			}
+		};
+	}
+	
+	
+}
