350d43b41681162c34b92c7ceace5df28b5527c0
Tomofumi Yuki <tomofumi.yuki@gmail.com>
2018-03-29 14:26:20 +0200

    removed old testframework impl

--- bundles/alpha.testframework/src/alpha/testframework/AlphaTransformation.java
+++
@@ -1,7 +0,0 @@
-package alpha.testframework;
-
-import alpha.model.AlphaRoot;
-import fr.irisa.cairn.gecos.testframework.ITransformation;
-
-@FunctionalInterface
-public interface AlphaTransformation extends ITransformation<AlphaRoot> {}
