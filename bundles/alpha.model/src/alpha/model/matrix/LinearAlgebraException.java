a10c18268003350c2f09cfa5e057ca38648556dc
Tomofumi Yuki <tomofumi.yuki@gmail.com>
2018-08-08 17:00:56 +0200

    added MatrixOperations (copied from old AlphaZ)

---
+++ bundles/alpha.model/src/alpha/model/matrix/LinearAlgebraException.java
@@ -0,0 +1,14 @@
+package alpha.model.matrix;
+
+public class LinearAlgebraException extends RuntimeException {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public LinearAlgebraException(String message) {
+		super(message);
+	}
+
+}
