6f356010668bbaaa78a64e39094d3dfebaea1c0f
Tomofumi Yuki <tomofumi.yuki@gmail.com>
2019-02-22 17:32:39 +0100

    [WIP] initial version of CoB

--- bundles/alpha.model/src/alpha/model/transformation/ChangeOfBasis.xtend
+++ bundles/alpha.model/src/alpha/model/transformation/ChangeOfBasis.xtend
@@ -3,16 +3,63 @@ package alpha.model.transformation
 import alpha.model.util.AbstractAlphaCompleteVisitor
 import alpha.model.AlphaSystem
 import fr.irisa.cairn.jnimap.isl.jni.JNIISLMultiAff
+import alpha.model.StandardEquation
+import alpha.model.UseEquation
+import alpha.model.VariableExpression
+import alpha.model.Variable
+import alpha.model.factory.AlphaUserFactory
+import alpha.model.matrix.factory.MatrixUserFactory
+import alpha.model.util.AffineFuntionOperations
+import fr.irisa.cairn.jnimap.isl.jni.JNIISLDimType
+import org.eclipse.emf.ecore.util.EcoreUtil
 
 class ChangeOfBasis extends AbstractAlphaCompleteVisitor {
 	
-	private new() {}
+	private new(AlphaSystem system, Variable variable, JNIISLMultiAff f) {
+		CoBfunction = f;
+		target = variable;
+		
+		CoBfunctionInverse = AffineFuntionOperations.inverseInContext(CoBfunction, null, f.space.getNameList(JNIISLDimType.isl_dim_in))
+	}
+	
+	JNIISLMultiAff CoBfunction;
+	JNIISLMultiAff CoBfunctionInverse;
+	Variable target;
 	
 	static def void apply(AlphaSystem system, String varName, JNIISLMultiAff f) {
 		//f.
 		
-		val CoB = new ChangeOfBasis;
+		val variable = system.variables.findFirst[v|v.name.contentEquals(varName)];
+		if (variable === null) throw new RuntimeException(String.format("Variable %s not found.", varName));
 		
+		val CoB = new ChangeOfBasis(system, variable, f);
+		system.accept(CoB);
+	}
+	
+	override inVariable(Variable variable) {
+		if (variable == target) {
+			val newDom = variable.domain.apply(CoBfunction.copy.toMap)
+			variable.domainExpr = AlphaUserFactory.createJNIDomain(newDom)
+		}
+	}
+	
+	override inStandardEquation(StandardEquation se) {
+		if (se.variable == target) {
+			val newExpr = AlphaUserFactory.createDependenceExpression(CoBfunctionInverse.copy, se.expr);
+			se.expr = newExpr
+		}
+	}
+	
+	override outVariableExpression(VariableExpression ve) {
+		if (ve.variable == target) {
+			val newExpr = AlphaUserFactory.createDependenceExpression(CoBfunction.copy);
+			EcoreUtil.replace(ve, newExpr);
+			newExpr.expr = ve
+		}
+	}
+		
+	override inUseEquation(UseEquation ue) {
+		super.inUseEquation(ue)	
 	}
 	
 }
--- bundles/alpha.model/xtend-gen/alpha/model/transformation/ChangeOfBasis.java
+++ bundles/alpha.model/xtend-gen/alpha/model/transformation/ChangeOfBasis.java
@@ -1,15 +1,81 @@
 package alpha.model.transformation;
 
 import alpha.model.AlphaSystem;
+import alpha.model.DependenceExpression;
+import alpha.model.StandardEquation;
+import alpha.model.UseEquation;
+import alpha.model.Variable;
+import alpha.model.VariableExpression;
+import alpha.model.factory.AlphaUserFactory;
 import alpha.model.util.AbstractAlphaCompleteVisitor;
+import alpha.model.util.AffineFuntionOperations;
+import com.google.common.base.Objects;
+import fr.irisa.cairn.jnimap.isl.jni.JNIISLDimType;
 import fr.irisa.cairn.jnimap.isl.jni.JNIISLMultiAff;
+import fr.irisa.cairn.jnimap.isl.jni.JNIISLSet;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.xtext.xbase.lib.Functions.Function1;
+import org.eclipse.xtext.xbase.lib.IterableExtensions;
 
 @SuppressWarnings("all")
 public class ChangeOfBasis extends AbstractAlphaCompleteVisitor {
-  private ChangeOfBasis() {
+  private ChangeOfBasis(final AlphaSystem system, final Variable variable, final JNIISLMultiAff f) {
+    this.CoBfunction = f;
+    this.target = variable;
+    this.CoBfunctionInverse = AffineFuntionOperations.inverseInContext(this.CoBfunction, null, f.getSpace().getNameList(JNIISLDimType.isl_dim_in));
   }
   
+  private JNIISLMultiAff CoBfunction;
+  
+  private JNIISLMultiAff CoBfunctionInverse;
+  
+  private Variable target;
+  
   public static void apply(final AlphaSystem system, final String varName, final JNIISLMultiAff f) {
-    final ChangeOfBasis CoB = new ChangeOfBasis();
+    final Function1<Variable, Boolean> _function = (Variable v) -> {
+      return Boolean.valueOf(v.getName().contentEquals(varName));
+    };
+    final Variable variable = IterableExtensions.<Variable>findFirst(system.getVariables(), _function);
+    if ((variable == null)) {
+      String _format = String.format("Variable %s not found.", varName);
+      throw new RuntimeException(_format);
+    }
+    final ChangeOfBasis CoB = new ChangeOfBasis(system, variable, f);
+    system.accept(CoB);
+  }
+  
+  @Override
+  public void inVariable(final Variable variable) {
+    boolean _equals = Objects.equal(variable, this.target);
+    if (_equals) {
+      final JNIISLSet newDom = variable.getDomain().apply(this.CoBfunction.copy().toMap());
+      variable.setDomainExpr(AlphaUserFactory.createJNIDomain(newDom));
+    }
+  }
+  
+  @Override
+  public void inStandardEquation(final StandardEquation se) {
+    Variable _variable = se.getVariable();
+    boolean _equals = Objects.equal(_variable, this.target);
+    if (_equals) {
+      final DependenceExpression newExpr = AlphaUserFactory.createDependenceExpression(this.CoBfunctionInverse.copy(), se.getExpr());
+      se.setExpr(newExpr);
+    }
+  }
+  
+  @Override
+  public void outVariableExpression(final VariableExpression ve) {
+    Variable _variable = ve.getVariable();
+    boolean _equals = Objects.equal(_variable, this.target);
+    if (_equals) {
+      final DependenceExpression newExpr = AlphaUserFactory.createDependenceExpression(this.CoBfunction.copy());
+      EcoreUtil.replace(ve, newExpr);
+      newExpr.setExpr(ve);
+    }
+  }
+  
+  @Override
+  public void inUseEquation(final UseEquation ue) {
+    super.inUseEquation(ue);
   }
 }
