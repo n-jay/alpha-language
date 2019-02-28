8f995c5b00f476840242993de958c240a84ee8d2
Tomofumi Yuki <tomofumi.yuki@gmail.com>
2019-02-28 17:33:22 +0100

    modified alpha.model.wizard to extend commands.ui

--- bundles/alpha.model.wizard/src/alpha/model/wizard/AlphaExampleWizard.java
+++ bundles/alpha.model.wizard/src/alpha/model/wizard/AlphaExampleWizard.java
@@ -3,21 +3,12 @@ package alpha.model.wizard;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
-import org.eclipse.core.resources.IFolder;
-import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.IWorkspaceRoot;
-import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.IProgressMonitor;
@@ -25,32 +16,25 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.Status;
-import org.eclipse.jdt.core.IClasspathEntry;
 import org.eclipse.jdt.core.IJavaProject;
-import org.eclipse.jdt.core.IPackageFragmentRoot;
-import org.eclipse.jdt.core.JavaCore;
-import org.eclipse.jdt.launching.JavaRuntime;
-import org.eclipse.jdt.launching.environments.IExecutionEnvironment;
-import org.eclipse.jdt.launching.environments.IExecutionEnvironmentsManager;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.operation.IRunnableWithProgress;
-import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.wizard.Wizard;
-import org.eclipse.ui.INewWizard;
-import org.eclipse.ui.IWorkbench;
 import org.osgi.framework.Bundle;
 
-public class AlphaExampleWizard extends Wizard implements INewWizard {
+import alpha.commands.ui.AlphaProjectWizard;
+
+public class AlphaExampleWizard extends AlphaProjectWizard {
 	
 	public static final String RESOURCES_ROOT = "resources/";
 
+	@Override
 	public String getPageName() { return "Alpha Example Project Wizard"; }
-	public String getPageDescription() { return "Creates a Java project with examples of Alpha programs and compiler scripts."; }
+
+	@Override
+	public String getPageDescription() { return "Creates a Java project with examples of Alpha programs and scripts."; }
+
+	@Override
 	protected Bundle getPluginBundle() {
 		return Platform.getBundle(Activator.PLUGIN_ID);
 	}
-	
-	private AlphaExampleWizardPage _page;
 	
 	public AlphaExampleWizard() {
 		super();
@@ -63,101 +47,18 @@ public class AlphaExampleWizard extends Wizard implements INewWizard {
 		addPage(_page);
 	};
 	
-	@Override
-	public void init(IWorkbench workbench, IStructuredSelection selection) {
-	}
-
-	@Override
-	public boolean performFinish() {
-		final String projectName = _page.getProjectName();
-		IRunnableWithProgress op = new IRunnableWithProgress() {
-			public void run(IProgressMonitor monitor) throws InvocationTargetException {
-				try {
-					doFinish(projectName, monitor);
-				} catch (CoreException e) {
-					throw new InvocationTargetException(e);
-				} finally {
-					monitor.done();
-				}
-			}
-		};
-		
-		try {
-			getContainer().run(true, false, op);
-		} catch (InterruptedException e) {
-			return false;
-		} catch (InvocationTargetException e) {
-			Throwable realException = e.getTargetException();
-			MessageDialog.openError(getShell(), "Error", realException.getMessage());
-			return false;
-		}
-		return true;
-	}
-	
 	/**
 	 * The worker method.
 	 */
 	protected void doFinish(String projectName, IProgressMonitor monitor) throws CoreException {
 		monitor.beginTask("Example project creation", IProgressMonitor.UNKNOWN);
-		IJavaProject project = createExampleProject(projectName, monitor);
+		IJavaProject project = createAlphaProject(projectName, monitor);
 		
 		copyResourcesToProjectFolder(project);
 		
 		monitor.worked(1);
 	}
 	
-	/**
-	 * Create the project with Xtext nature
-	 * @param projectName
-	 * @param monitor
-	 * @return
-	 * @throws CoreException
-	 */
-	private IJavaProject createExampleProject(String projectName, IProgressMonitor monitor) throws CoreException {
-		IWorkspaceRoot wkRoot = ResourcesPlugin.getWorkspace().getRoot();
-		IProject project = wkRoot.getProject(projectName);
-		
-		if(Arrays.asList(wkRoot.getProjects()).contains(project)){
-			IStatus status = new Status(Status.ERROR, getPluginBundle().getSymbolicName(), "Project with the same name already exist in the workspace");
-			throw new CoreException(status);
-		}
-		
-		project.create(null);
-		project.open(null);
-		
-		IProjectDescription projectDescription = ResourcesPlugin.getWorkspace().newProjectDescription(project.getName());
-		projectDescription.setNatureIds(new String[] { JavaCore.NATURE_ID,  "org.eclipse.xtext.ui.shared.xtextNature" });
-		project.setDescription(projectDescription, null);
-		
-		IJavaProject javaProject = JavaCore.create(project);
-		
-
-		IFolder binFolder = project.getFolder("bin");
-		if (!binFolder.exists())
-			binFolder.create(false, true, null);
-		javaProject.setOutputLocation(binFolder.getFullPath(), null);
-		
-		IFolder sourceFolder = project.getFolder("src");
-		if (!sourceFolder.exists())
-			sourceFolder.create(false, true, null);
-		
-		List<IClasspathEntry> entries = new ArrayList<IClasspathEntry>();
-		IExecutionEnvironmentsManager executionEnvironmentsManager = JavaRuntime.getExecutionEnvironmentsManager();
-		IExecutionEnvironment[] executionEnvironments = executionEnvironmentsManager.getExecutionEnvironments();
-		for (IExecutionEnvironment iExecutionEnvironment : executionEnvironments) {
-		    if ("JavaSE-1.8".equals(iExecutionEnvironment.getId())) {
-		        entries.add(JavaCore.newContainerEntry(JavaRuntime.newJREContainerPath(iExecutionEnvironment)));
-		        break;
-		    }
-		}
-
-		IPackageFragmentRoot root = javaProject.getPackageFragmentRoot(sourceFolder);
-		entries.add(JavaCore.newSourceEntry(root.getPath()));
-
-		javaProject.setRawClasspath(entries.toArray(new IClasspathEntry[entries.size()]), null);		
-
-		return javaProject;
-	}
 	
 	private void copyResourcesToProjectFolder(IJavaProject project) throws CoreException {
 		Bundle bundle = getPluginBundle();
--- bundles/alpha.model.wizard/src/alpha/model/wizard/AlphaExampleWizardPage.java
+++ bundles/alpha.model.wizard/src/alpha/model/wizard/AlphaExampleWizardPage.java
@@ -1,74 +1,17 @@
 package alpha.model.wizard;
 
-import org.eclipse.jface.wizard.WizardPage;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.ModifyEvent;
-import org.eclipse.swt.events.ModifyListener;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Text;
+import alpha.commands.ui.AlphaProjectWizardPage;
 
-public class AlphaExampleWizardPage extends WizardPage {
-		private static final String DEFAULT_PROJECT_NAME = "alpha.examples";
-	
-		private Text _projectName;
+public class AlphaExampleWizardPage extends AlphaProjectWizardPage {
 
-		public AlphaExampleWizardPage(String pageName, String description) {
-			super(pageName);
-			setTitle(pageName);
-			setDescription(description);
-		}
-
-		@Override
-		public void createControl(Composite parent) {
-			Composite container = new Composite(parent, SWT.NULL);
-			GridLayout layout = new GridLayout();
-			container.setLayout(layout);
-			layout.numColumns = 3;
-			layout.verticalSpacing = 9;
-			GridData gd = new GridData(GridData.FILL_HORIZONTAL);
-
-			Label label = new Label(container, SWT.NULL);
-			label.setText("&Project name:");
-
-			_projectName = new Text(container, SWT.BORDER | SWT.SINGLE);
-			_projectName.setText(DEFAULT_PROJECT_NAME);
-			gd = new GridData(GridData.FILL_HORIZONTAL);
-			_projectName.setLayoutData(gd);
-			_projectName.addModifyListener(new ModifyListener() {
-				public void modifyText(ModifyEvent e) {
-					dialogChanged();
-				}
-			});
-		
-			dialogChanged();
-			setControl(container);
-		}
-		
-		private void dialogChanged() {
-
-			String projectName = _projectName.getText();
-			if (projectName.length() == 0) {
-				updateStatus("Project name must be specified");
-				return;
-			}
-			if (projectName.replace('\\', '/').indexOf('/', 1) > 0) {
-				updateStatus("File name must be valid");
-				return;
-			}
-		
-			updateStatus(null);
-		}
-		
-		private void updateStatus(String message) {
-			setErrorMessage(message);
-			setPageComplete(message == null);
-		}
-		
-		public String getProjectName(){
-			return _projectName.getText();
-		}
+	public AlphaExampleWizardPage(String pageName, String description) {
+		super(pageName, description);
 	}
+
+	@Override
+	protected String getDefaultProjectName() {
+		return "alpha.examples";
+	}
+	
+}
 
