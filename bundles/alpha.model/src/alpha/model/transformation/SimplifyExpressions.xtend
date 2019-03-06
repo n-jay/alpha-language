cf40b051fe7cf94ce0853c158528381600aa0690
Tomofumi Yuki <tomofumi.yuki@gmail.com>
2019-03-06 14:53:59 +0100

    added SimplifyExpressions

---
+++ bundles/alpha.model/src/alpha/model/transformation/SimplifyExpressions.xtend
@@ -0,0 +1,111 @@
+package alpha.model.transformation
+
+import alpha.model.AlphaCompleteVisitable
+import alpha.model.AlphaExpression
+import alpha.model.AlphaExpressionVisitable
+import alpha.model.AlphaVisitable
+import alpha.model.BINARY_OP
+import alpha.model.BinaryExpression
+import alpha.model.IndexExpression
+import alpha.model.ReduceExpression
+import alpha.model.factory.AlphaUserFactory
+import alpha.model.util.AbstractAlphaCompleteVisitor
+import alpha.model.util.AlphaOperatorUtil
+import alpha.model.util.AlphaUtil
+import alpha.model.util.Show
+import fr.irisa.cairn.jnimap.isl.jni.ISLFactory
+import fr.irisa.cairn.jnimap.isl.jni.JNIISLDimType
+import org.eclipse.emf.ecore.util.EcoreUtil
+import fr.irisa.cairn.jnimap.isl.jni.JNIISLAff
+import fr.irisa.cairn.jnimap.isl.jni.JNIISLPWAff
+import alpha.model.ExpressionDomainCalculator
+import alpha.model.ContextDomainCalculator
+import alpha.model.AlphaInternalStateConstructor
+
+/**
+ * Simplification of Alpha programs.
+ * 
+ * This visitor has a similar structure to Normalize. The main difference is that
+ * the transformations in Simplify are not necessary for a program to be in normal form.
+ * Normalize also implements transformations that are not strictly necessary for
+ * normal form, but eliminate errors. For instance, removing empty case branches 
+ * is necessary to avoid errors after Normalize.
+ * 
+ * The simplification rules implemented are:
+ *  - remove BinaryExpression when one of the operand is identity
+ *  - replace IndexExpression with ConstantExpression when applicable
+ *  - replace ReduceExpression with its body when it has scalar domain
+ */
+class SimplifyExpressions extends AbstractAlphaCompleteVisitor {
+	
+	
+	static boolean DEBUG = false
+	
+	protected def debug(String ruleID, String rule) {
+		if (DEBUG) println(ruleID + ": " + rule)
+	}
+	
+	protected def debug(AlphaExpression expr) {
+		if (DEBUG) println(Show.print(AlphaUtil.getContainerSystem(expr))); 
+	}
+	
+	protected new() {}
+
+	static def void apply(AlphaCompleteVisitable acv) {
+		if (acv instanceof AlphaVisitable) {
+			apply(acv as AlphaVisitable)
+		} else if (acv instanceof AlphaExpressionVisitable) {
+			apply(acv as AlphaExpressionVisitable)
+		} else {
+			throw new RuntimeException("This should be unreachable code. Got: " + acv);
+		}
+	}
+	
+	static def void apply(AlphaVisitable av) {
+		val visitor = new SimplifyExpressions();
+		av.accept(visitor);
+	}
+	static def void apply(AlphaExpressionVisitable aev) {
+		val visitor = new SimplifyExpressions();
+		aev.accept(visitor);
+	}
+	
+	override outBinaryExpression(BinaryExpression be) {
+		if (AlphaOperatorUtil.isIdentity(be.operator, be.left) && be.operator != BINARY_OP.DIV && be.operator != BINARY_OP.MOD) {
+			EcoreUtil.replace(be, be.right);
+		} else if (AlphaOperatorUtil.isIdentity(be.operator, be.right)) {
+			EcoreUtil.replace(be, be.left);
+		}
+	}
+	
+	override outIndexExpression(IndexExpression ie) {
+		if (ie.function.nbAff == 1 && ie.function.getAff(0).isConstant) {
+			val v = ie.function.getAff(0).eval(ie.expressionDomain.copy.samplePoint)
+			val intExpr = AlphaUserFactory.createIntegerExpression(v.asLong.intValue);
+			EcoreUtil.replace(ie, intExpr)
+		}
+	}
+	
+		override outReduceExpression(ReduceExpression re) {
+			var map = re.projection.toMap
+			map = map.intersectDomain(re.body.contextDomain)
+			map = map.intersectRange(re.contextDomain)
+			map = map.reverse
+
+			if (map.isSingleValued) {
+				val pwMultiAff = map.toPWMultiAff
+				
+				//TODO it can be extended to multiple pieces using case
+				if (pwMultiAff.nbPieces == 1) {
+					val multiAff = pwMultiAff.getPiece(0).maff
+					val depExpr = AlphaUserFactory.createDependenceExpression(multiAff, re.body)
+					EcoreUtil.replace(re, depExpr)
+					
+					AlphaInternalStateConstructor.recomputeContextDomain(depExpr)
+				} else {
+					System.err.println("[Simplify] Scalar Reduction with multiple pieces detected. It is not simplified with the currnent implementation	.")
+				}
+			}
+		}
+	
+}
---
+++ bundles/alpha.model/xtend-gen/alpha/model/transformation/SimplifyExpressions.java
@@ -0,0 +1,129 @@
+package alpha.model.transformation;
+
+import alpha.model.AlphaCompleteVisitable;
+import alpha.model.AlphaExpression;
+import alpha.model.AlphaExpressionVisitable;
+import alpha.model.AlphaInternalStateConstructor;
+import alpha.model.AlphaSystem;
+import alpha.model.AlphaVisitable;
+import alpha.model.BINARY_OP;
+import alpha.model.BinaryExpression;
+import alpha.model.DependenceExpression;
+import alpha.model.IndexExpression;
+import alpha.model.IntegerExpression;
+import alpha.model.ReduceExpression;
+import alpha.model.factory.AlphaUserFactory;
+import alpha.model.util.AbstractAlphaCompleteVisitor;
+import alpha.model.util.AlphaOperatorUtil;
+import alpha.model.util.AlphaUtil;
+import alpha.model.util.Show;
+import com.google.common.base.Objects;
+import fr.irisa.cairn.jnimap.isl.jni.JNIISLMap;
+import fr.irisa.cairn.jnimap.isl.jni.JNIISLMultiAff;
+import fr.irisa.cairn.jnimap.isl.jni.JNIISLPWMultiAff;
+import fr.irisa.cairn.jnimap.isl.jni.JNIISLVal;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.xtext.xbase.lib.InputOutput;
+
+/**
+ * Simplification of Alpha programs.
+ * 
+ * This visitor has a similar structure to Normalize. The main difference is that
+ * the transformations in Simplify are not necessary for a program to be in normal form.
+ * Normalize also implements transformations that are not strictly necessary for
+ * normal form, but eliminate errors. For instance, removing empty case branches
+ * is necessary to avoid errors after Normalize.
+ * 
+ * The simplification rules implemented are:
+ *  - remove BinaryExpression when one of the operand is identity
+ *  - replace IndexExpression with ConstantExpression when applicable
+ *  - replace ReduceExpression with its body when it has scalar domain
+ */
+@SuppressWarnings("all")
+public class SimplifyExpressions extends AbstractAlphaCompleteVisitor {
+  private static boolean DEBUG = false;
+  
+  protected String debug(final String ruleID, final String rule) {
+    String _xifexpression = null;
+    if (SimplifyExpressions.DEBUG) {
+      _xifexpression = InputOutput.<String>println(((ruleID + ": ") + rule));
+    }
+    return _xifexpression;
+  }
+  
+  protected String debug(final AlphaExpression expr) {
+    String _xifexpression = null;
+    if (SimplifyExpressions.DEBUG) {
+      _xifexpression = InputOutput.<String>println(Show.<AlphaSystem>print(AlphaUtil.getContainerSystem(expr)));
+    }
+    return _xifexpression;
+  }
+  
+  protected SimplifyExpressions() {
+  }
+  
+  public static void apply(final AlphaCompleteVisitable acv) {
+    if ((acv instanceof AlphaVisitable)) {
+      SimplifyExpressions.apply(((AlphaVisitable) acv));
+    } else {
+      if ((acv instanceof AlphaExpressionVisitable)) {
+        SimplifyExpressions.apply(((AlphaExpressionVisitable) acv));
+      } else {
+        throw new RuntimeException(("This should be unreachable code. Got: " + acv));
+      }
+    }
+  }
+  
+  public static void apply(final AlphaVisitable av) {
+    final SimplifyExpressions visitor = new SimplifyExpressions();
+    av.accept(visitor);
+  }
+  
+  public static void apply(final AlphaExpressionVisitable aev) {
+    final SimplifyExpressions visitor = new SimplifyExpressions();
+    aev.accept(visitor);
+  }
+  
+  @Override
+  public void outBinaryExpression(final BinaryExpression be) {
+    if (((AlphaOperatorUtil.isIdentity(be.getOperator(), be.getLeft()) && (!Objects.equal(be.getOperator(), BINARY_OP.DIV))) && (!Objects.equal(be.getOperator(), BINARY_OP.MOD)))) {
+      EcoreUtil.replace(be, be.getRight());
+    } else {
+      boolean _isIdentity = AlphaOperatorUtil.isIdentity(be.getOperator(), be.getRight());
+      if (_isIdentity) {
+        EcoreUtil.replace(be, be.getLeft());
+      }
+    }
+  }
+  
+  @Override
+  public void outIndexExpression(final IndexExpression ie) {
+    if (((ie.getFunction().getNbAff() == 1) && ie.getFunction().getAff(0).isConstant())) {
+      final JNIISLVal v = ie.getFunction().getAff(0).eval(ie.getExpressionDomain().copy().samplePoint());
+      final IntegerExpression intExpr = AlphaUserFactory.createIntegerExpression(Long.valueOf(v.asLong()).intValue());
+      EcoreUtil.replace(ie, intExpr);
+    }
+  }
+  
+  @Override
+  public void outReduceExpression(final ReduceExpression re) {
+    JNIISLMap map = re.getProjection().toMap();
+    map = map.intersectDomain(re.getBody().getContextDomain());
+    map = map.intersectRange(re.getContextDomain());
+    map = map.reverse();
+    boolean _isSingleValued = map.isSingleValued();
+    if (_isSingleValued) {
+      final JNIISLPWMultiAff pwMultiAff = map.toPWMultiAff();
+      int _nbPieces = pwMultiAff.getNbPieces();
+      boolean _equals = (_nbPieces == 1);
+      if (_equals) {
+        final JNIISLMultiAff multiAff = pwMultiAff.getPiece(0).getMaff();
+        final DependenceExpression depExpr = AlphaUserFactory.createDependenceExpression(multiAff, re.getBody());
+        EcoreUtil.replace(re, depExpr);
+        AlphaInternalStateConstructor.recomputeContextDomain(depExpr);
+      } else {
+        System.err.println("[Simplify] Scalar Reduction with multiple pieces detected. It is not simplified with the currnent implementation\t.");
+      }
+    }
+  }
+}
