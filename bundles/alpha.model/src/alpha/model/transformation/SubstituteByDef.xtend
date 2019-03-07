1509fe950b402f2066d6b4bd1488b32a014a2393
Tomofumi Yuki <tomofumi.yuki@gmail.com>
2019-03-07 15:01:36 +0100

    added SubstituteByDef

--- bundles/alpha.model/src/alpha/model/transformation/SubstituteByDef.xtend
+++ bundles/alpha.model/src/alpha/model/transformation/SubstituteByDef.xtend
@@ -2,26 +2,81 @@ package alpha.model.transformation
 
 import alpha.model.AlphaSystem
 import alpha.model.StandardEquation
+import alpha.model.SystemBody
+import alpha.model.Variable
 import alpha.model.util.AbstractAlphaCompleteVisitor
+import alpha.model.UseEquation
+import alpha.model.VariableExpression
+import alpha.model.factory.AlphaUserFactory
+import org.eclipse.emf.ecore.util.EcoreUtil
+import alpha.model.AlphaInternalStateConstructor
 
+/**
+ * Replaces a VariableExpression with the definition of the corresponding StandardEquation.
+ * Variables defined through UseEquations cannot be substituted.
+ * 
+ * When the target equation is specified, the substitution only happens in the definition
+ * of the target. Otherwise, all occurrences in an AlphaSystem (or SystemBody) are substituted.
+ * 
+ * Given
+ *  X = Y;
+ *  Y = E;
+ * substituting Y in X gives:
+ *  X = D : E
+ * where D is the domain of Y. 
+ * 
+ */
 class SubstituteByDef extends AbstractAlphaCompleteVisitor  {
-	protected StandardEquation transformedEquation
-	protected StandardEquation substitutedEquation
+	protected SystemBody systemBody;
+	protected StandardEquation targetEquation
+	protected StandardEquation substituteEquation
 	
-	private new(AlphaSystem system, StandardEquation transformedEquation, StandardEquation substitutedEquation) {
-		this.transformedEquation = transformedEquation
-		this.substitutedEquation = substitutedEquation
+	private new(SystemBody systemBody, StandardEquation targetEquation, StandardEquation substituteEquation) {
+		this.systemBody = systemBody;
+		this.targetEquation = targetEquation
+		this.substituteEquation = substituteEquation
 	}
 	
 	
-	static def void apply(AlphaSystem system, String varName, String varToSubstitute) {
-		
-		//TODO Get StandardEquation objects from system
-		
-		//TODO create instance of this class
-		
-		//TODO call accept
+	static def void apply(AlphaSystem system, Variable substituteVar) {
+		system.systemBodies.forEach[body|apply(body, substituteVar)]
 	}
 	
-	//TODO override visitor methods to implement substitution
+	static def void apply(AlphaSystem system, StandardEquation targetEq, Variable substituteVar) {
+		system.systemBodies.forEach[body|apply(body, targetEq, substituteVar)]
+	}
+	
+	static def void apply(SystemBody body, Variable substituteVar) {
+		apply(body, null, substituteVar)
+	}
+	
+	static def void apply(SystemBody body, StandardEquation targetEq, Variable substituteVar) {
+		
+		val subEq = body.getStandardEquation(substituteVar)
+		val visitor = new SubstituteByDef(body, targetEq, subEq)
+		
+		visitor.accept(body)
+	}
+	
+	override visitUseEquation(UseEquation ue) {
+		//stop visiting
+		return
+	}
+	
+	override visitStandardEquation(StandardEquation se) {
+		//only visit targets
+		if (targetEquation === null || se == targetEquation)
+			super.visitStandardEquation(se)
+		return
+	}
+	
+	override outVariableExpression(VariableExpression ve) {
+		if (ve.variable == substituteEquation.variable) {
+			val substituteExpr = AlphaUserFactory.createRestrictExpression(substituteEquation.variable.domain, EcoreUtil.copy(substituteEquation.expr))
+			EcoreUtil.replace(ve, substituteExpr)
+			AlphaInternalStateConstructor.recomputeContextDomain(substituteExpr)
+		}
+	}
+
+	
 }
--- bundles/alpha.model/xtend-gen/alpha/model/transformation/SubstituteByDef.java
+++ bundles/alpha.model/xtend-gen/alpha/model/transformation/SubstituteByDef.java
@@ -1,20 +1,94 @@
 package alpha.model.transformation;
 
+import alpha.model.AlphaExpression;
+import alpha.model.AlphaInternalStateConstructor;
 import alpha.model.AlphaSystem;
+import alpha.model.RestrictExpression;
 import alpha.model.StandardEquation;
+import alpha.model.SystemBody;
+import alpha.model.UseEquation;
+import alpha.model.Variable;
+import alpha.model.VariableExpression;
+import alpha.model.factory.AlphaUserFactory;
 import alpha.model.util.AbstractAlphaCompleteVisitor;
+import com.google.common.base.Objects;
+import java.util.function.Consumer;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 
+/**
+ * Replaces a VariableExpression with the definition of the corresponding StandardEquation.
+ * Variables defined through UseEquations cannot be substituted.
+ * 
+ * When the target equation is specified, the substitution only happens in the definition
+ * of the target. Otherwise, all occurrences in an AlphaSystem (or SystemBody) are substituted.
+ * 
+ * Given
+ *  X = Y;
+ *  Y = E;
+ * substituting Y in X gives:
+ *  X = D : E
+ * where D is the domain of Y.
+ */
 @SuppressWarnings("all")
 public class SubstituteByDef extends AbstractAlphaCompleteVisitor {
-  protected StandardEquation transformedEquation;
+  protected SystemBody systemBody;
   
-  protected StandardEquation substitutedEquation;
+  protected StandardEquation targetEquation;
   
-  private SubstituteByDef(final AlphaSystem system, final StandardEquation transformedEquation, final StandardEquation substitutedEquation) {
-    this.transformedEquation = transformedEquation;
-    this.substitutedEquation = substitutedEquation;
+  protected StandardEquation substituteEquation;
+  
+  private SubstituteByDef(final SystemBody systemBody, final StandardEquation targetEquation, final StandardEquation substituteEquation) {
+    this.systemBody = systemBody;
+    this.targetEquation = targetEquation;
+    this.substituteEquation = substituteEquation;
   }
   
-  public static void apply(final AlphaSystem system, final String varName, final String varToSubstitute) {
+  public static void apply(final AlphaSystem system, final Variable substituteVar) {
+    final Consumer<SystemBody> _function = (SystemBody body) -> {
+      SubstituteByDef.apply(body, substituteVar);
+    };
+    system.getSystemBodies().forEach(_function);
+  }
+  
+  public static void apply(final AlphaSystem system, final StandardEquation targetEq, final Variable substituteVar) {
+    final Consumer<SystemBody> _function = (SystemBody body) -> {
+      SubstituteByDef.apply(body, targetEq, substituteVar);
+    };
+    system.getSystemBodies().forEach(_function);
+  }
+  
+  public static void apply(final SystemBody body, final Variable substituteVar) {
+    SubstituteByDef.apply(body, null, substituteVar);
+  }
+  
+  public static void apply(final SystemBody body, final StandardEquation targetEq, final Variable substituteVar) {
+    final StandardEquation subEq = body.getStandardEquation(substituteVar);
+    final SubstituteByDef visitor = new SubstituteByDef(body, targetEq, subEq);
+    visitor.accept(body);
+  }
+  
+  @Override
+  public void visitUseEquation(final UseEquation ue) {
+    return;
+  }
+  
+  @Override
+  public void visitStandardEquation(final StandardEquation se) {
+    if (((this.targetEquation == null) || Objects.equal(se, this.targetEquation))) {
+      super.visitStandardEquation(se);
+    }
+    return;
+  }
+  
+  @Override
+  public void outVariableExpression(final VariableExpression ve) {
+    Variable _variable = ve.getVariable();
+    Variable _variable_1 = this.substituteEquation.getVariable();
+    boolean _equals = Objects.equal(_variable, _variable_1);
+    if (_equals) {
+      final RestrictExpression substituteExpr = AlphaUserFactory.createRestrictExpression(this.substituteEquation.getVariable().getDomain(), EcoreUtil.<AlphaExpression>copy(this.substituteEquation.getExpr()));
+      EcoreUtil.replace(ve, substituteExpr);
+      AlphaInternalStateConstructor.recomputeContextDomain(substituteExpr);
+    }
   }
 }
