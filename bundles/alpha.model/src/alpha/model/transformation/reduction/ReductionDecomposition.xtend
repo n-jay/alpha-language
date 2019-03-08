42d17271df968876fa8ad8b1c444926966a0f067
Tomofumi Yuki <tomofumi.yuki@gmail.com>
2019-03-08 11:23:48 +0100

    added ReductionDecomposition

---
+++ bundles/alpha.model/src/alpha/model/transformation/reduction/ReductionDecomposition.xtend
@@ -0,0 +1,41 @@
+package alpha.model.transformation.reduction
+
+import alpha.model.AbstractReduceExpression
+import alpha.model.factory.AlphaUserFactory
+import fr.irisa.cairn.jnimap.isl.jni.ISLErrorException
+import fr.irisa.cairn.jnimap.isl.jni.JNIISLMultiAff
+import fr.irisa.cairn.jnimap.isl.jni.JNIISLTools
+
+/**
+ * ReductionDecomposition splits a reduction into two.
+ * A reduction of the form:
+ *   reduce(op, f, E)
+ * is transformed into:
+ *   reduce(op, f2, reduce(op, f1, E))
+ * where f = f2 o f1.
+ */
+class ReductionDecomposition {
+	
+	private new() {}
+	
+	/**
+	 * Applies ReductionDecomposition using the input functions.
+	 * Input f1 becomes the projection function of the inner reduction.
+	 */
+	static def apply(AbstractReduceExpression are, JNIISLMultiAff f1, JNIISLMultiAff f2) {
+		val validDecomposition = {
+			try {
+				JNIISLTools.recordError([|f2.copy.pullback(f1.copy).isPlainEqual(are.projection)])
+			} catch (ISLErrorException isle) {
+				throw new IllegalArgumentException("[ReductionDecomposition] ISL error while testing for validity of input functions: " + isle.islErrorMessage);
+			}
+		}
+		if (!validDecomposition) {
+			throw new IllegalArgumentException("[ReductionDecomposition] The composition of input functions must match the original projection. ");
+		}
+		
+		val innerARE = AlphaUserFactory.createReduceExpression(are.operator, f1, are.body)
+		are.body = innerARE
+		are.projectionExpr = AlphaUserFactory.createJNIFunction(f2)
+	}
+}
---
+++ bundles/alpha.model/xtend-gen/alpha/model/transformation/reduction/ReductionDecomposition.java
@@ -0,0 +1,52 @@
+package alpha.model.transformation.reduction;
+
+import alpha.model.AbstractReduceExpression;
+import alpha.model.ReduceExpression;
+import alpha.model.factory.AlphaUserFactory;
+import fr.irisa.cairn.jnimap.isl.jni.ISLErrorException;
+import fr.irisa.cairn.jnimap.isl.jni.JNIISLMultiAff;
+import fr.irisa.cairn.jnimap.isl.jni.JNIISLTools;
+import java.util.function.Supplier;
+import org.eclipse.xtext.xbase.lib.Exceptions;
+
+/**
+ * ReductionDecomposition splits a reduction into two.
+ * A reduction of the form:
+ *   reduce(op, f, E)
+ * is transformed into:
+ *   reduce(op, f2, reduce(op, f1, E))
+ * where f = f2 o f1.
+ */
+@SuppressWarnings("all")
+public class ReductionDecomposition {
+  private ReductionDecomposition() {
+  }
+  
+  /**
+   * Applies ReductionDecomposition using the input functions.
+   * Input f1 becomes the projection function of the inner reduction.
+   */
+  public static void apply(final AbstractReduceExpression are, final JNIISLMultiAff f1, final JNIISLMultiAff f2) {
+    Boolean _xtrycatchfinallyexpression = null;
+    try {
+      final Supplier<Boolean> _function = () -> {
+        return Boolean.valueOf(f2.copy().pullback(f1.copy()).isPlainEqual(are.getProjection()));
+      };
+      _xtrycatchfinallyexpression = JNIISLTools.<Boolean>recordError(_function);
+    } catch (final Throwable _t) {
+      if (_t instanceof ISLErrorException) {
+        final ISLErrorException isle = (ISLErrorException)_t;
+        throw new IllegalArgumentException(("[ReductionDecomposition] ISL error while testing for validity of input functions: " + isle.islErrorMessage));
+      } else {
+        throw Exceptions.sneakyThrow(_t);
+      }
+    }
+    final Boolean validDecomposition = _xtrycatchfinallyexpression;
+    if ((!(validDecomposition).booleanValue())) {
+      throw new IllegalArgumentException("[ReductionDecomposition] The composition of input functions must match the original projection. ");
+    }
+    final ReduceExpression innerARE = AlphaUserFactory.createReduceExpression(are.getOperator(), f1, are.getBody());
+    are.setBody(innerARE);
+    are.setProjectionExpr(AlphaUserFactory.createJNIFunction(f2));
+  }
+}
