50f419f681cdd3ae596f9586c2a27213e99b0f81
Tomofumi Yuki <tomofumi.yuki@gmail.com>
2019-03-13 18:08:23 +0100

    forgot to commit AlphaSystemElement

---
+++ bundles/alpha.model/src-gen/alpha/model/AlphaSystemElement.java
@@ -0,0 +1,18 @@
+/**
+ */
+package alpha.model;
+
+import org.eclipse.emf.ecore.EObject;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Alpha System Element</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see alpha.model.ModelPackage#getAlphaSystemElement()
+ * @model interface="true" abstract="true"
+ * @generated
+ */
+public interface AlphaSystemElement extends EObject {
+} // AlphaSystemElement
