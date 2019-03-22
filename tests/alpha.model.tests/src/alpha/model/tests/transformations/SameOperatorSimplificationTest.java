dd767d5b16db00116e6c34dbd149fdc282b388ad
Tomofumi Yuki <tomofumi.yuki@gmail.com>
2019-03-22 14:58:59 +0100

    added SameOperatorSimplificationTest

---
+++ tests/alpha.model.tests/src/alpha/model/tests/transformations/SameOperatorSimplificationTest.java
@@ -0,0 +1,49 @@
+package alpha.model.tests.transformations;
+
+import java.util.Collection;
+
+import org.eclipse.xtext.EcoreUtil2;
+import org.junit.Assert;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import alpha.model.AbstractReduceExpression;
+import alpha.model.AlphaSystem;
+import alpha.model.BinaryExpression;
+import alpha.model.MultiArgExpression;
+import alpha.model.StandardEquation;
+import alpha.model.tests.GenericAlphaSystemTest;
+import alpha.model.tests.data.IAlphaTestInput;
+import alpha.model.transformation.reduction.SameOperatorSimplification;
+
+@RunWith(Parameterized.class)
+public class SameOperatorSimplificationTest extends GenericAlphaSystemTest {
+
+	private static final String ROOT_DIR = "resources/src-valid/transformation-tests/same-operator-simplification/";
+	
+	public SameOperatorSimplificationTest(final IAlphaTestInput test) {
+		super(test);
+	}
+	
+	@Parameters(name="{0}")
+	public static Collection<Object[]> data() {
+		return getData(ROOT_DIR);
+	}
+	
+	@Override
+	protected void doTest(AlphaSystem system) {
+		EcoreUtil2.getAllContentsOfType(system, StandardEquation.class).
+		forEach(x->{
+			Assert.assertTrue(
+					"SameOperatorSimplificationTest assumes first expression of any StandardEquation is a reduction.",
+					x.getExpr() instanceof AbstractReduceExpression);
+			SameOperatorSimplification.apply((AbstractReduceExpression)x.getExpr());
+		});
+		
+		EcoreUtil2.getAllContentsOfType(system, AbstractReduceExpression.class).
+		forEach(x->Assert.assertTrue(
+				EcoreUtil2.getAllContentsOfType(x, BinaryExpression.class).isEmpty() &&
+				EcoreUtil2.getAllContentsOfType(x, MultiArgExpression.class).isEmpty()));
+	}
+}
