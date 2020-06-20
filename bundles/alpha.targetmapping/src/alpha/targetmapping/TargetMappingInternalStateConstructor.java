5c892a236fba9173484d809f008007246398c84c
Tomofumi Yuki <tomofumi.yuki@gmail.com>
2020-06-20 09:01:37 +0200

    stub classes in targetmapping/src to have something in src

---
+++ bundles/alpha.targetmapping/src/alpha/targetmapping/TargetMappingInternalStateConstructor.java
@@ -0,0 +1,8 @@
+package alpha.targetmapping;
+
+import fr.irisa.cairn.jnimap.isl.ISLASTLoopType;
+
+public class TargetMappingInternalStateConstructor {
+
+	private ISLASTLoopType looptype = ISLASTLoopType.isl_ast_loop_atomic; 
+}
---
+++ bundles/alpha.targetmapping/src/alpha/targetmapping/issue/TargetMappingIssue.java
@@ -0,0 +1,30 @@
+package alpha.targetmapping.issue;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.xtext.validation.AbstractDeclarativeValidator;
+
+/**
+ * TargetMappingIssue is intended to report errors during IR construction to Xtext editor.
+ * 
+ * It is eventually mapped to:<ul>
+ *   <li>{@link AbstractDeclarativeValidator.error}</li>
+ *   <li>{@link AbstractDeclarativeValidator.warning}</li> 
+ * </ul>
+ * depending on the TYPE.
+ * 
+ * 
+ * @author tyuki
+ *
+ */
+public interface TargetMappingIssue {
+	public enum TYPE {
+		WARNING,
+		ERROR
+	}
+
+	public TYPE getType();
+	public String getMessage();
+	public EObject getSource();
+	public EStructuralFeature getFeature();
+}
