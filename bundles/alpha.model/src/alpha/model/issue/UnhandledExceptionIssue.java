9868491635454ff2028753c40d2dec016cd1974e
Tomofumi Yuki <tomofumi.yuki@gmail.com>
2020-06-23 15:34:54 +0200

    added more generic constructor for UnhandledExceptionIssue (to be used by TargetMapping)

--- bundles/alpha.model/src/alpha/model/issue/UnhandledExceptionIssue.java
+++ bundles/alpha.model/src/alpha/model/issue/UnhandledExceptionIssue.java
@@ -1,10 +1,17 @@
 package alpha.model.issue;
 
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
+
 import alpha.model.AlphaRoot;
 import alpha.model.ModelPackage;
 
 public class UnhandledExceptionIssue extends AbstractAlphaIssue {
 
+	public UnhandledExceptionIssue(EObject source, EStructuralFeature feature, RuntimeException re) {
+		super(TYPE.ERROR, String.format("Unhandled exception during parsing: %s", re.getMessage()), source, feature);
+	}
+	
 	public UnhandledExceptionIssue(AlphaRoot root, RuntimeException re) {
 		super(TYPE.ERROR, String.format("Unhandled exception during parsing: %s", re.getMessage()), root, ModelPackage.Literals.ALPHA_ROOT__ELEMENTS);
 	}
