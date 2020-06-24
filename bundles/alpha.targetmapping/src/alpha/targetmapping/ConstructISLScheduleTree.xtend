06dd0511d351c17c831b37600e8c43c57a26d6bd
Tomofumi Yuki <tomofumi.yuki@gmail.com>
2020-06-24 18:13:15 +0200

    first version of ConstructISLScheduleTree

---
+++ bundles/alpha.targetmapping/src/alpha/targetmapping/ConstructISLScheduleTree.xtend
@@ -0,0 +1,283 @@
+package alpha.targetmapping
+
+import alpha.targetmapping.util.AbstractTargetMappingVisitor
+import fr.irisa.cairn.jnimap.isl.ISLSchedule
+import org.eclipse.xtext.EcoreUtil2
+import alpha.model.AlphaScheduleTarget
+import alpha.model.StandardEquation
+import alpha.model.UseEquation
+import fr.irisa.cairn.jnimap.isl.ISLUnionSet
+import alpha.targetmapping.util.TargetMappingUtil
+import java.util.List
+import fr.irisa.cairn.jnimap.isl.ISLUnionSetList
+import fr.irisa.cairn.jnimap.isl.ISLContext
+import java.util.LinkedList
+import fr.irisa.cairn.jnimap.isl.ISLIdentifier
+import fr.irisa.cairn.jnimap.isl.ISLDimType
+import fr.irisa.cairn.jnimap.isl.ISLMap
+import fr.irisa.cairn.jnimap.isl.ISLMultiPWAff
+import fr.irisa.cairn.jnimap.isl.ISLException
+import fr.irisa.cairn.jnimap.isl.ISLScheduleExtensionNode
+import fr.irisa.cairn.jnimap.isl.ISLScheduleSequenceNode
+import java.util.Map
+import java.util.HashMap
+
+class ConstructISLScheduleTree extends AbstractTargetMappingVisitor {
+	
+	private new() {}
+	
+	ISLSchedule schedule
+	List<Integer> path
+	
+	/**
+	 * This visitor maintains an up-to-date copy of ISLSchedule, and keeps
+	 * track of the path to the node where the insertion of the next 
+	 * node in the TM tree should be inserted as the tree is traversed.
+	 * 
+	 */
+	def getCurrentNode() {
+		var node = schedule.root
+		for (p : path) {
+			node = node.getChild(p)
+		}
+		return node
+	}
+
+	def static ISLSchedule construct(TargetMappingForSystemBody tm) {
+		val visitor = new ConstructISLScheduleTree();
+		
+		visitor.accept(tm);
+		
+		return visitor.schedule;
+	}
+	
+	
+	override void inTargetMappingForSystemBody(TargetMappingForSystemBody tm) {
+		super.inTargetMappingForSystemBody(tm);
+		
+		val targets = EcoreUtil2.getAllContentsOfType(tm.getTargetBody(), AlphaScheduleTarget);
+		
+		var domain = targets.map[t|t.constructNamedSet.toUnionSet].reduce[s1, s2|s1.union(s2)]
+		
+		schedule = ISLSchedule.buildFromDomain(domain)
+		path = new LinkedList();
+		
+		path.add(0);
+		
+		//ContextExpression is only allowed at the root (and is optional)
+		// if context expression is not the first expression, then add parameter domain as context
+		// otherwise, parameter domain is intersected with the context domain at ContextExpression
+		// adding 0 dimensional set dim is necessary
+		// what is excepted as context in ISL sechedule tree is the space of parameter + outer scheduled dims
+		// at the root, the outer scheduled dims is always 0 dimensional, but the space must still live in 
+		// set space not parameters
+		if (!(tm.scheduleTreeRoot instanceof ContextExpression)) {
+			val paramDomain = tm.targetBody.system.parameterDomain;
+			val contextDomain = paramDomain.addDims(ISLDimType.isl_dim_set, 0)
+			schedule = schedule.insertContext(contextDomain)
+			path.add(0);
+		}
+	}
+	
+	override inContextExpression(ContextExpression ce) {
+		//specify the intersection of context domain and the parameter domain as context
+		val pDom = TargetMappingUtil.getTargetSystem(ce).parameterDomain
+		schedule = schedule.insertContext(ce.contextDomain.intersectParams(pDom))
+		
+		path.add(0);
+	}
+	
+	override outContextExpression(ContextExpression ce) {
+		path.remove(path.size-1)
+	}
+	
+	override visitSequenceExpression(SequenceExpression se) {
+		val filters = se.children.filter[c|c instanceof FilterExpression]
+		val domains = filters.map[fe|(fe as FilterExpression).constructDomain]
+		var unionSetList = ISLUnionSetList.build(ISLContext.getInstance(), domains.size);
+		
+		for (d : domains)
+			unionSetList = unionSetList.add(d)
+			
+		var seqNode = currentNode.insertSequence(unionSetList)
+		println(seqNode)
+		
+		//Sequence may have extensions as first and/or last children. In such cases, 
+		// the extension maps are grafted either before or after this sequence.
+		val extensions = EcoreUtil2.getObjectsByType(se.children, TargetmappingPackage.Literals.EXTENSION_EXPRESSION)
+		if (extensions.size > 1) {
+			for (ex : extensions) {
+				val exMap = (ex as ExtensionExpression).constructNamedExtensionMap
+				val exNode = ISLScheduleExtensionNode.buildFromExtensionMap(exMap)
+				
+				//select graft before or after
+				if (se.children.indexOf(ex) == 0) {
+					val graftedNode = seqNode.graftBefore(exNode)
+					seqNode = graftedNode as ISLScheduleSequenceNode
+				} else if (se.children.indexOf(ex) == se.children.size-1) {
+					val graftedNode = seqNode.graftAfter(exNode)
+					seqNode = graftedNode as ISLScheduleSequenceNode
+				} else {
+					throw new RuntimeException("ExtensionExpression is not at the beginning/end of sequence")
+				}
+			}
+			seqNode = (seqNode.parent.parent as ISLScheduleSequenceNode).spliceChild(1)
+			
+			path.add(0)
+		}
+		
+		schedule = seqNode.schedule
+		
+		val depth = path.size
+		path.add(0)
+		
+		for (c : se.children) {
+			path.set(depth, se.children.indexOf(c))
+			c.accept(this)
+		}
+		
+		path.remove(depth)
+		
+		if (extensions.size> 0) {
+			path.remove(depth-1)
+		} 
+	}
+	
+	override visitSetExpression(SetExpression se) {
+		val domains = se.children.map[c|(c as FilterExpression).constructDomain]
+		var unionSetList = ISLUnionSetList.build(ISLContext.getInstance(), domains.size);
+		
+		for (d : domains)
+			unionSetList = unionSetList.add(d)
+			
+		val seqNode = currentNode.insertSequence(unionSetList)
+		schedule = seqNode.schedule
+		
+		val depth = path.size
+		path.add(0)
+		
+		for (c : se.children) {
+			path.set(depth, se.children.indexOf(c))
+			c.accept(this)
+		}
+		
+		path.remove(depth)
+	}
+	
+	override inFilterExpression(FilterExpression fe) {
+		//If the parent was Set/Seq, then filter is already created
+		if (fe.eContainer instanceof SetExpression || fe.eContainer instanceof SequenceExpression) {
+			path.add(0)
+			return
+		}
+		
+		val filterDomain = fe.constructDomain
+		val filterNode = currentNode.insertFilter(filterDomain)
+		schedule = filterNode.schedule
+		
+		path.add(0)
+	}
+	
+	override outFilterExpression(FilterExpression fe) {
+		path.remove(path.size-1)
+	}
+	
+	override inGuardExpression(GuardExpression ge) {
+		val guardNode = currentNode.insertGuard(ge.guardDomain)
+		schedule = guardNode.schedule
+		
+		path.add(0)
+	}
+	
+	override outGuardExpression(GuardExpression ge) {
+		path.remove(path.size-1)
+	}
+	
+	override inMarkExpression(MarkExpression me) {
+		val markNode = currentNode.insertMark(ISLIdentifier.alloc(ISLContext.instance, me.identifier))
+		schedule = markNode.schedule
+		
+		path.add(0)
+	}
+	
+	override outMarkExpression(MarkExpression me) {
+		path.remove(path.size-1)
+	}
+	
+	override inBandExpression(BandExpression be) {
+		val partialSchedule = be.bandPieces.map[constructPartialSchedule].reduce[p1, p2|println(p1); println(p2); p1.unionAdd(p2)]
+		var bandNode = currentNode.insertPartialSchedule(partialSchedule)
+		
+		for (lts : be.loopTypeSpecifications) {
+			bandNode = bandNode.setASTLoopType(lts.dimension, lts.loopType)
+		}
+		
+		if (be.isolateSpecification !== null) {
+			val map = ISLMap.buildFromRange(be.isolateSpecification.isolateDomain)
+			val isolateOption = map.wrap.tupleName = "isolate"
+			bandNode = bandNode.ASTBuildOptions = isolateOption.toUnionSet
+			
+			for (lts : be.isolateSpecification.loopTypeSpecifications) {
+				bandNode = bandNode.setIsolateASTLoopType(lts.dimension, lts.loopType)
+			}
+		}
+		
+		schedule = bandNode.schedule
+		path.add(0)
+	}
+	
+	override outBandExpression(BandExpression be) {
+		path.remove(path.size-1)
+	}
+	
+	
+	//ExtensionExpressions are handled at SetExpression
+	// it translates as filter node in the tree, so the paths are updated accordingly
+	override inExtensionExpression(ExtensionExpression ee) {
+		path.add(0)
+	}
+	
+	override outExtensionExpression(ExtensionExpression ee) {
+		path.remove(path.size-1)
+	}
+	
+	private def dispatch constructNamedSet(StandardEquation eq) {
+		val vDom = eq.variable.domain
+		return vDom.tupleName = eq.name
+	}
+	private def dispatch constructNamedSet(UseEquation eq) {
+		//use equation should use instantiation domain -- not implemented yet
+		throw new UnsupportedOperationException();
+	}
+	private def dispatch constructNamedSet(ExtensionTarget et) {
+		val range = et.extensionMap.getRange
+		return range.tupleName = et.name
+	}
+	
+	private def constructNamedSet(ScheduleTargetRestrictDomain strd) {
+		return strd.restrictDomain.tupleName = strd.scheduleTarget.name
+	}
+	
+	private def constructDomain(FilterExpression fe) {
+		return fe.filterDomains.map[constructNamedSet.toUnionSet].reduce[s1, s2|s1.union(s2)]
+	}
+	
+	private def constructPartialSchedule(BandPiece bp) {
+		val pieceDomain = bp.pieceDomain.constructNamedSet
+		var partialSchedule = bp.partialSchedule.toMultiPWAff
+		partialSchedule = partialSchedule.setTupleName(ISLDimType.isl_dim_in, bp.pieceDomain.scheduleTarget.name)
+		partialSchedule = partialSchedule.intersectDomain(pieceDomain)
+		partialSchedule.toMultiUnionPWAff
+	}
+	
+	private def constructNamedExtensionMap(ExtensionExpression ee) {
+		return ee.extensionTargets.map[constructNamedExtensionMap].reduce[p1, p2|p1.union(p2)]
+	}
+	
+	private def constructNamedExtensionMap(ExtensionTarget et) {
+		var stExMap = et.extensionMap.outputTupleName = et.name
+		if (et.source !== null)
+			stExMap = stExMap.inputTupleName = et.source.name
+		return (stExMap as ISLMap).toUnionMap
+	}
+}
---
+++ bundles/alpha.targetmapping/xtend-gen/alpha/targetmapping/ConstructISLScheduleTree.java
@@ -0,0 +1,387 @@
+package alpha.targetmapping;
+
+import alpha.model.AlphaScheduleTarget;
+import alpha.model.StandardEquation;
+import alpha.model.UseEquation;
+import alpha.targetmapping.BandExpression;
+import alpha.targetmapping.BandPiece;
+import alpha.targetmapping.ContextExpression;
+import alpha.targetmapping.ExtensionExpression;
+import alpha.targetmapping.ExtensionTarget;
+import alpha.targetmapping.FilterExpression;
+import alpha.targetmapping.GuardExpression;
+import alpha.targetmapping.IsolateSpecification;
+import alpha.targetmapping.LoopTypeSpecification;
+import alpha.targetmapping.MarkExpression;
+import alpha.targetmapping.ScheduleTargetRestrictDomain;
+import alpha.targetmapping.ScheduleTreeExpression;
+import alpha.targetmapping.SequenceExpression;
+import alpha.targetmapping.SetExpression;
+import alpha.targetmapping.TargetMappingForSystemBody;
+import alpha.targetmapping.TargetmappingPackage;
+import alpha.targetmapping.util.AbstractTargetMappingVisitor;
+import alpha.targetmapping.util.TargetMappingUtil;
+import fr.irisa.cairn.jnimap.isl.IISLSingleSpaceMapMethods;
+import fr.irisa.cairn.jnimap.isl.ISLContext;
+import fr.irisa.cairn.jnimap.isl.ISLDimType;
+import fr.irisa.cairn.jnimap.isl.ISLIdentifier;
+import fr.irisa.cairn.jnimap.isl.ISLMap;
+import fr.irisa.cairn.jnimap.isl.ISLMultiPWAff;
+import fr.irisa.cairn.jnimap.isl.ISLMultiUnionPWAff;
+import fr.irisa.cairn.jnimap.isl.ISLSchedule;
+import fr.irisa.cairn.jnimap.isl.ISLScheduleBandNode;
+import fr.irisa.cairn.jnimap.isl.ISLScheduleExtensionNode;
+import fr.irisa.cairn.jnimap.isl.ISLScheduleFilterNode;
+import fr.irisa.cairn.jnimap.isl.ISLScheduleGuardNode;
+import fr.irisa.cairn.jnimap.isl.ISLScheduleMarkNode;
+import fr.irisa.cairn.jnimap.isl.ISLScheduleNode;
+import fr.irisa.cairn.jnimap.isl.ISLScheduleSequenceNode;
+import fr.irisa.cairn.jnimap.isl.ISLSet;
+import fr.irisa.cairn.jnimap.isl.ISLUnionMap;
+import fr.irisa.cairn.jnimap.isl.ISLUnionSet;
+import fr.irisa.cairn.jnimap.isl.ISLUnionSetList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.xtext.EcoreUtil2;
+import org.eclipse.xtext.xbase.lib.Functions.Function1;
+import org.eclipse.xtext.xbase.lib.Functions.Function2;
+import org.eclipse.xtext.xbase.lib.InputOutput;
+import org.eclipse.xtext.xbase.lib.IterableExtensions;
+import org.eclipse.xtext.xbase.lib.ListExtensions;
+
+@SuppressWarnings("all")
+public class ConstructISLScheduleTree extends AbstractTargetMappingVisitor {
+  private ConstructISLScheduleTree() {
+  }
+  
+  private ISLSchedule schedule;
+  
+  private List<Integer> path;
+  
+  /**
+   * This visitor maintains an up-to-date copy of ISLSchedule, and keeps
+   * track of the path to the node where the insertion of the next
+   * node in the TM tree should be inserted as the tree is traversed.
+   */
+  public ISLScheduleNode getCurrentNode() {
+    ISLScheduleNode node = this.schedule.getRoot();
+    for (final Integer p : this.path) {
+      node = node.getChild((p).intValue());
+    }
+    return node;
+  }
+  
+  public static ISLSchedule construct(final TargetMappingForSystemBody tm) {
+    final ConstructISLScheduleTree visitor = new ConstructISLScheduleTree();
+    visitor.accept(tm);
+    return visitor.schedule;
+  }
+  
+  @Override
+  public void inTargetMappingForSystemBody(final TargetMappingForSystemBody tm) {
+    super.inTargetMappingForSystemBody(tm);
+    final List<AlphaScheduleTarget> targets = EcoreUtil2.<AlphaScheduleTarget>getAllContentsOfType(tm.getTargetBody(), AlphaScheduleTarget.class);
+    final Function1<AlphaScheduleTarget, ISLUnionSet> _function = (AlphaScheduleTarget t) -> {
+      return this.constructNamedSet(t).toUnionSet();
+    };
+    final Function2<ISLUnionSet, ISLUnionSet, ISLUnionSet> _function_1 = (ISLUnionSet s1, ISLUnionSet s2) -> {
+      return s1.union(s2);
+    };
+    ISLUnionSet domain = IterableExtensions.<ISLUnionSet>reduce(ListExtensions.<AlphaScheduleTarget, ISLUnionSet>map(targets, _function), _function_1);
+    this.schedule = ISLSchedule.buildFromDomain(domain);
+    LinkedList<Integer> _linkedList = new LinkedList<Integer>();
+    this.path = _linkedList;
+    this.path.add(Integer.valueOf(0));
+    ScheduleTreeExpression _scheduleTreeRoot = tm.getScheduleTreeRoot();
+    boolean _not = (!(_scheduleTreeRoot instanceof ContextExpression));
+    if (_not) {
+      final ISLSet paramDomain = tm.getTargetBody().getSystem().getParameterDomain();
+      final ISLSet contextDomain = paramDomain.addDims(ISLDimType.isl_dim_set, 0);
+      this.schedule = this.schedule.insertContext(contextDomain);
+      this.path.add(Integer.valueOf(0));
+    }
+  }
+  
+  @Override
+  public void inContextExpression(final ContextExpression ce) {
+    final ISLSet pDom = TargetMappingUtil.getTargetSystem(ce).getParameterDomain();
+    this.schedule = this.schedule.insertContext(ce.getContextDomain().intersectParams(pDom));
+    this.path.add(Integer.valueOf(0));
+  }
+  
+  @Override
+  public void outContextExpression(final ContextExpression ce) {
+    int _size = this.path.size();
+    int _minus = (_size - 1);
+    this.path.remove(_minus);
+  }
+  
+  @Override
+  public void visitSequenceExpression(final SequenceExpression se) {
+    final Function1<ScheduleTreeExpression, Boolean> _function = (ScheduleTreeExpression c) -> {
+      return Boolean.valueOf((c instanceof FilterExpression));
+    };
+    final Iterable<ScheduleTreeExpression> filters = IterableExtensions.<ScheduleTreeExpression>filter(se.getChildren(), _function);
+    final Function1<ScheduleTreeExpression, ISLUnionSet> _function_1 = (ScheduleTreeExpression fe) -> {
+      return this.constructDomain(((FilterExpression) fe));
+    };
+    final Iterable<ISLUnionSet> domains = IterableExtensions.<ScheduleTreeExpression, ISLUnionSet>map(filters, _function_1);
+    ISLUnionSetList unionSetList = ISLUnionSetList.build(ISLContext.getInstance(), IterableExtensions.size(domains));
+    for (final ISLUnionSet d : domains) {
+      unionSetList = unionSetList.add(d);
+    }
+    ISLScheduleSequenceNode seqNode = this.getCurrentNode().insertSequence(unionSetList);
+    InputOutput.<ISLScheduleSequenceNode>println(seqNode);
+    final Collection<Object> extensions = EcoreUtil2.<Object>getObjectsByType(se.getChildren(), TargetmappingPackage.Literals.EXTENSION_EXPRESSION);
+    int _size = extensions.size();
+    boolean _greaterThan = (_size > 1);
+    if (_greaterThan) {
+      for (final Object ex : extensions) {
+        {
+          final ISLUnionMap exMap = this.constructNamedExtensionMap(((ExtensionExpression) ex));
+          final ISLScheduleExtensionNode exNode = ISLScheduleExtensionNode.buildFromExtensionMap(exMap);
+          int _indexOf = se.getChildren().indexOf(ex);
+          boolean _equals = (_indexOf == 0);
+          if (_equals) {
+            final ISLScheduleNode graftedNode = seqNode.graftBefore(exNode);
+            seqNode = ((ISLScheduleSequenceNode) graftedNode);
+          } else {
+            int _indexOf_1 = se.getChildren().indexOf(ex);
+            int _size_1 = se.getChildren().size();
+            int _minus = (_size_1 - 1);
+            boolean _equals_1 = (_indexOf_1 == _minus);
+            if (_equals_1) {
+              final ISLScheduleNode graftedNode_1 = seqNode.graftAfter(exNode);
+              seqNode = ((ISLScheduleSequenceNode) graftedNode_1);
+            } else {
+              throw new RuntimeException("ExtensionExpression is not at the beginning/end of sequence");
+            }
+          }
+        }
+      }
+      ISLScheduleNode _parent = seqNode.parent().parent();
+      seqNode = ((ISLScheduleSequenceNode) _parent).spliceChild(1);
+      this.path.add(Integer.valueOf(0));
+    }
+    this.schedule = seqNode.getSchedule();
+    final int depth = this.path.size();
+    this.path.add(Integer.valueOf(0));
+    EList<ScheduleTreeExpression> _children = se.getChildren();
+    for (final ScheduleTreeExpression c : _children) {
+      {
+        this.path.set(depth, Integer.valueOf(se.getChildren().indexOf(c)));
+        c.accept(this);
+      }
+    }
+    this.path.remove(depth);
+    int _size_1 = extensions.size();
+    boolean _greaterThan_1 = (_size_1 > 0);
+    if (_greaterThan_1) {
+      this.path.remove((depth - 1));
+    }
+  }
+  
+  @Override
+  public void visitSetExpression(final SetExpression se) {
+    final Function1<ScheduleTreeExpression, ISLUnionSet> _function = (ScheduleTreeExpression c) -> {
+      return this.constructDomain(((FilterExpression) c));
+    };
+    final List<ISLUnionSet> domains = ListExtensions.<ScheduleTreeExpression, ISLUnionSet>map(se.getChildren(), _function);
+    ISLUnionSetList unionSetList = ISLUnionSetList.build(ISLContext.getInstance(), domains.size());
+    for (final ISLUnionSet d : domains) {
+      unionSetList = unionSetList.add(d);
+    }
+    final ISLScheduleSequenceNode seqNode = this.getCurrentNode().insertSequence(unionSetList);
+    this.schedule = seqNode.getSchedule();
+    final int depth = this.path.size();
+    this.path.add(Integer.valueOf(0));
+    EList<ScheduleTreeExpression> _children = se.getChildren();
+    for (final ScheduleTreeExpression c : _children) {
+      {
+        this.path.set(depth, Integer.valueOf(se.getChildren().indexOf(c)));
+        c.accept(this);
+      }
+    }
+    this.path.remove(depth);
+  }
+  
+  @Override
+  public void inFilterExpression(final FilterExpression fe) {
+    if (((fe.eContainer() instanceof SetExpression) || (fe.eContainer() instanceof SequenceExpression))) {
+      this.path.add(Integer.valueOf(0));
+      return;
+    }
+    final ISLUnionSet filterDomain = this.constructDomain(fe);
+    final ISLScheduleFilterNode filterNode = this.getCurrentNode().insertFilter(filterDomain);
+    this.schedule = filterNode.getSchedule();
+    this.path.add(Integer.valueOf(0));
+  }
+  
+  @Override
+  public void outFilterExpression(final FilterExpression fe) {
+    int _size = this.path.size();
+    int _minus = (_size - 1);
+    this.path.remove(_minus);
+  }
+  
+  @Override
+  public void inGuardExpression(final GuardExpression ge) {
+    final ISLScheduleGuardNode guardNode = this.getCurrentNode().insertGuard(ge.getGuardDomain());
+    this.schedule = guardNode.getSchedule();
+    this.path.add(Integer.valueOf(0));
+  }
+  
+  @Override
+  public void outGuardExpression(final GuardExpression ge) {
+    int _size = this.path.size();
+    int _minus = (_size - 1);
+    this.path.remove(_minus);
+  }
+  
+  @Override
+  public void inMarkExpression(final MarkExpression me) {
+    final ISLScheduleMarkNode markNode = this.getCurrentNode().insertMark(ISLIdentifier.alloc(ISLContext.getInstance(), me.getIdentifier()));
+    this.schedule = markNode.getSchedule();
+    this.path.add(Integer.valueOf(0));
+  }
+  
+  @Override
+  public void outMarkExpression(final MarkExpression me) {
+    int _size = this.path.size();
+    int _minus = (_size - 1);
+    this.path.remove(_minus);
+  }
+  
+  @Override
+  public void inBandExpression(final BandExpression be) {
+    final Function1<BandPiece, ISLMultiUnionPWAff> _function = (BandPiece it) -> {
+      return this.constructPartialSchedule(it);
+    };
+    final Function2<ISLMultiUnionPWAff, ISLMultiUnionPWAff, ISLMultiUnionPWAff> _function_1 = (ISLMultiUnionPWAff p1, ISLMultiUnionPWAff p2) -> {
+      ISLMultiUnionPWAff _xblockexpression = null;
+      {
+        InputOutput.<ISLMultiUnionPWAff>println(p1);
+        InputOutput.<ISLMultiUnionPWAff>println(p2);
+        _xblockexpression = p1.unionAdd(p2);
+      }
+      return _xblockexpression;
+    };
+    final ISLMultiUnionPWAff partialSchedule = IterableExtensions.<ISLMultiUnionPWAff>reduce(ListExtensions.<BandPiece, ISLMultiUnionPWAff>map(be.getBandPieces(), _function), _function_1);
+    ISLScheduleBandNode bandNode = this.getCurrentNode().insertPartialSchedule(partialSchedule);
+    EList<LoopTypeSpecification> _loopTypeSpecifications = be.getLoopTypeSpecifications();
+    for (final LoopTypeSpecification lts : _loopTypeSpecifications) {
+      bandNode = bandNode.setASTLoopType(lts.getDimension(), lts.getLoopType());
+    }
+    IsolateSpecification _isolateSpecification = be.getIsolateSpecification();
+    boolean _tripleNotEquals = (_isolateSpecification != null);
+    if (_tripleNotEquals) {
+      final ISLMap map = ISLMap.buildFromRange(be.getIsolateSpecification().getIsolateDomain());
+      ISLSet _wrap = map.wrap();
+      final ISLSet isolateOption = _wrap.setTupleName("isolate");
+      bandNode = bandNode.setASTBuildOptions(isolateOption.toUnionSet());
+      EList<LoopTypeSpecification> _loopTypeSpecifications_1 = be.getIsolateSpecification().getLoopTypeSpecifications();
+      for (final LoopTypeSpecification lts_1 : _loopTypeSpecifications_1) {
+        bandNode = bandNode.setIsolateASTLoopType(lts_1.getDimension(), lts_1.getLoopType());
+      }
+    }
+    this.schedule = bandNode.getSchedule();
+    this.path.add(Integer.valueOf(0));
+  }
+  
+  @Override
+  public void outBandExpression(final BandExpression be) {
+    int _size = this.path.size();
+    int _minus = (_size - 1);
+    this.path.remove(_minus);
+  }
+  
+  @Override
+  public void inExtensionExpression(final ExtensionExpression ee) {
+    this.path.add(Integer.valueOf(0));
+  }
+  
+  @Override
+  public void outExtensionExpression(final ExtensionExpression ee) {
+    int _size = this.path.size();
+    int _minus = (_size - 1);
+    this.path.remove(_minus);
+  }
+  
+  private ISLSet _constructNamedSet(final StandardEquation eq) {
+    final ISLSet vDom = eq.getVariable().getDomain();
+    return vDom.setTupleName(eq.getName());
+  }
+  
+  private ISLSet _constructNamedSet(final UseEquation eq) {
+    throw new UnsupportedOperationException();
+  }
+  
+  private ISLSet _constructNamedSet(final ExtensionTarget et) {
+    final ISLSet range = et.getExtensionMap().getRange();
+    return range.setTupleName(et.getName());
+  }
+  
+  private ISLSet constructNamedSet(final ScheduleTargetRestrictDomain strd) {
+    ISLSet _restrictDomain = strd.getRestrictDomain();
+    return _restrictDomain.setTupleName(strd.getScheduleTarget().getName());
+  }
+  
+  private ISLUnionSet constructDomain(final FilterExpression fe) {
+    final Function1<ScheduleTargetRestrictDomain, ISLUnionSet> _function = (ScheduleTargetRestrictDomain it) -> {
+      return this.constructNamedSet(it).toUnionSet();
+    };
+    final Function2<ISLUnionSet, ISLUnionSet, ISLUnionSet> _function_1 = (ISLUnionSet s1, ISLUnionSet s2) -> {
+      return s1.union(s2);
+    };
+    return IterableExtensions.<ISLUnionSet>reduce(ListExtensions.<ScheduleTargetRestrictDomain, ISLUnionSet>map(fe.getFilterDomains(), _function), _function_1);
+  }
+  
+  private ISLMultiUnionPWAff constructPartialSchedule(final BandPiece bp) {
+    ISLMultiUnionPWAff _xblockexpression = null;
+    {
+      final ISLSet pieceDomain = this.constructNamedSet(bp.getPieceDomain());
+      ISLMultiPWAff partialSchedule = bp.getPartialSchedule().toMultiPWAff();
+      partialSchedule = partialSchedule.setTupleName(ISLDimType.isl_dim_in, bp.getPieceDomain().getScheduleTarget().getName());
+      partialSchedule = partialSchedule.intersectDomain(pieceDomain);
+      _xblockexpression = partialSchedule.toMultiUnionPWAff();
+    }
+    return _xblockexpression;
+  }
+  
+  private ISLUnionMap constructNamedExtensionMap(final ExtensionExpression ee) {
+    final Function1<ExtensionTarget, ISLUnionMap> _function = (ExtensionTarget it) -> {
+      return this.constructNamedExtensionMap(it);
+    };
+    final Function2<ISLUnionMap, ISLUnionMap, ISLUnionMap> _function_1 = (ISLUnionMap p1, ISLUnionMap p2) -> {
+      return p1.union(p2);
+    };
+    return IterableExtensions.<ISLUnionMap>reduce(ListExtensions.<ExtensionTarget, ISLUnionMap>map(ee.getExtensionTargets(), _function), _function_1);
+  }
+  
+  private ISLUnionMap constructNamedExtensionMap(final ExtensionTarget et) {
+    ISLMap _extensionMap = et.getExtensionMap();
+    IISLSingleSpaceMapMethods stExMap = _extensionMap.setOutputTupleName(et.getName());
+    AlphaScheduleTarget _source = et.getSource();
+    boolean _tripleNotEquals = (_source != null);
+    if (_tripleNotEquals) {
+      stExMap = stExMap.setInputTupleName(et.getSource().getName());
+    }
+    return ((ISLMap) stExMap).toUnionMap();
+  }
+  
+  private ISLSet constructNamedSet(final AlphaScheduleTarget eq) {
+    if (eq instanceof StandardEquation) {
+      return _constructNamedSet((StandardEquation)eq);
+    } else if (eq instanceof UseEquation) {
+      return _constructNamedSet((UseEquation)eq);
+    } else if (eq instanceof ExtensionTarget) {
+      return _constructNamedSet((ExtensionTarget)eq);
+    } else {
+      throw new IllegalArgumentException("Unhandled parameter types: " +
+        Arrays.<Object>asList(eq).toString());
+    }
+  }
+}
